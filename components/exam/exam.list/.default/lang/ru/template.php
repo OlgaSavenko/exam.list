e05e9a260c1aa9857b66b4d535da44005cccbbc1
Olga Savenko <kaira-88@mail.ru>
2022-02-09 15:38:18 +0300

    Add files via upload

---
+++ components/exam/exam.list/.default/lang/ru/template.php
@@ -0,0 +1,7 @@
+<?
+	$MESS['EXAM_LIST_TEMPLATE_TITLE'] = 'Расписание экзаменов';
+	$MESS['EXAM_LIST_TEMPLATE_NO_ITEMS'] = 'Не найдено ни одного экзамена';
+	$MESS['EXAM_LIST_TEMPLATE_EXAM_DATE'] = 'Дата экзамена';
+	$MESS['EXAM_LIST_TEMPLATE_EXAM_NAME'] = 'Название предмета';
+	$MESS['EXAM_LIST_TEMPLATE_TEACHER_NAME'] = 'Преподаватель';
+	$MESS['EXAM_LIST_TEMPLATE_ROOM'] = 'Аудитория';
