6eb9316b59393342d118b698b2e53989453173f9
Olga Savenko <kaira-88@mail.ru>
2022-02-09 15:45:20 +0300

    Update component.php

--- components/exam/exam.list/component.php
+++ components/exam/exam.list/component.php
@@ -6,11 +6,11 @@
 		
 	try {
 		if (!Main\Loader::includeModule('iblock')) {
-	        throw new Main\LoaderException(Loc::getMessage('EXAM_LIST_COMPONENT_IBLOCK_MODULE_NOT_INSTALLED'));
-	    }
-	    
-	    //if ($this->StartResultCache()) {
-		    $dbResult = CIBlockElement::GetList(
+			throw new Main\LoaderException(Loc::getMessage('EXAM_LIST_COMPONENT_IBLOCK_MODULE_NOT_INSTALLED'));
+	    	}
+		
+		if ($this->StartResultCache()) {
+			$dbResult = CIBlockElement::GetList(
 		    	Array('DATE_ACTIVE_FROM'=>'ASC'), 
 				Array('IBLOCK_ID'=>$arParams["IBLOCK_ID"]),
 			);
@@ -20,7 +20,7 @@
 				$arResult['ITEMS'][$fields['ID']] = $fields;
 				$arResult['ITEMS'][$fields['ID']]['PROPERTIES'] = $ob->GetProperties();
 			}
-		//}
+		}
 		
 		// подключаем шаблон компонента
 		$this->IncludeComponentTemplate();
